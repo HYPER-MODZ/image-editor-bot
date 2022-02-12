685a12d845815eaf8b096222d0458a8f3d8bf473
Lakshitha <87607134+HYPER-MODZ@users.noreply.github.com>
2022-02-12 21:16:56 +0530

    image editor bot by hyper mod

---
+++ script.py
@@ -0,0 +1,26 @@
+class script(object):
+
+    START_MSG = """ <b>Hi {}
+
+I'm a Image Editor Bot which Supports various modes
+
+For more click help....</b>"""
+    HELP_MSG = """Hai, Follow these Steps..
+
+<code>🌀 Send me any Image to Edit..</code>
+
+<code>🌀 Select the Corresponding mode that you need</code>
+
+<code>🌀 Your Edited Image will be Uploaded </code>
+
+
+© @HYPER-MOD"""
+    ABOUT_MSG = """⭕️<b>My Name : Image Editor Bot</b>
+
+⭕️<b>Language :</b> <code>Python3</code>
+
+⭕️<b>Library :</b> <a href='https://docs.pyrogram.org/'>Pyrogram 1.0.7</a>
+
+⭕️<b>Youtube :</b> <a href='https://youtube.com/c/HYPERMOD'>Subscribe</a>
+
+⭕️<b>Source Code :</b> 👉 <a href='https://github.com/HYPER-MOD/image-editor-bot'>Click Here</a>"""
